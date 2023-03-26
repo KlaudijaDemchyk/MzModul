bd4d55659e778e750b1d58540f5522fa1a46e30c
KlaudijaDemchyk <fkst@i.ua>
2023-03-26 14:11:56 +0200

    change modul

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import {validateForm} from 'kch-checkform/Valid-Check-Form';
+import {validateForm} from 'kch-checkform/function-valid-form';
 
-document.getElementById("button").addEventListener("click",checkForm());
-    function checkForm() {
+document.getElementById("button").addEventListener("click",checkForm);
+   export function checkForm() {
         const nameInput = document.getElementById('name');
         const passwordInput = document.getElementById('password');
         if (validateForm(nameInput, passwordInput)) {
